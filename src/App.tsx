3faae71423cd9768ccb88cd7ac2db09831e83d0f
Mati1609 <165424818+Mati1609@users.noreply.github.com>
2026-05-31 21:51:59 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -85,8 +85,8 @@ const AvatarDisplay = ({ config, className = "" }: { config: AvatarConfig, class
         'Corona': { top: '-2%', width: '38%', left: '25%', zIndex: 20 },
         'Lentes claros': { top: '32%', width: '52%', left: '20%', zIndex: 25 },
         'Lentes oscuros': { top: '32%', width: '52%', left: '20%', zIndex: 25 },
-        'Bufanda': { bottom: '5%', width: '56%', left: '15%', zIndex: 20 },
-        'Humita': { bottom: '1%', width: '28%', left: '34%', zIndex: 20 }
+        'Bufanda': { bottom: '5%', width: '56%', left: '17%', zIndex: 20 },
+        'Humita': { bottom: '5%', width: '28%', left: '34%', zIndex: 20 }
       },
       panda: {
         'Sombrero': { top: '3%', width: '58%', left: '21%', zIndex: 20 },
